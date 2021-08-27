4f665dc04e0384e092bcf7185af5bf82a75e92f5
Swapnil Shinde <48307487+swapnil3925@users.noreply.github.com>
2021-08-27 12:19:12 +0530

    initial commit

---
+++ IfStatement.java
@@ -0,0 +1,19 @@
+package conditionalOperator;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		int a=6;
+		if(a % 2 == 0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+
+	}
+
+}
