fd73d8b8a09d06e55a121b6be8add8c4a2348c05
Swapnil Shinde <48307487+swapnil3925@users.noreply.github.com>
2021-08-27 12:34:45 +0530

    add string variable

--- IfStatement.java
+++ IfStatement.java
@@ -4,6 +4,11 @@ public class IfStatement {
 
 	public static void main(String[] args) {
 		int a=6;
+
+		String name="swapnil shinde";
+
+		String address="sangli";
+		
 		if(a % 2 == 0)
 		{
 			System.out.println("even");
